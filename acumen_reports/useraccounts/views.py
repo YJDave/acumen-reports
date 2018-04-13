b6504ff54c5ba47530f8bb86541ceb02fedd4714
YJDave <yashashvidave@gmail.com>
2018-04-13 14:31:03 +0530

    templates: Add home template, inheriting base template.

--- acumen_reports/useraccounts/views.py
+++ acumen_reports/useraccounts/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth import views
 
 class HomePage(views.TemplateView):
-	template_name = 'base.html'
+	template_name = 'useraccounts/home.html'
 	pass
 
 class UserDashboard(views.TemplateView):
