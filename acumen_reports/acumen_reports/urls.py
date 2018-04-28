ad7ffd7078aa1bb7ea5974efbfb3a9aa43695cd9
Parthvi Vala <parthvi.vala@gmail.com>
2018-04-28 15:37:03 +0530

    mdl for django forms

--- acumen_reports/acumen_reports/urls.py
+++ acumen_reports/acumen_reports/urls.py
@@ -17,11 +17,13 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from useraccounts.views.home import HomePage
 from useraccounts.views.user_accounts import RegisterView
+from allauth.account import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', HomePage.as_view(), name="home"),
-    url(r'^accounts/signup/$', RegisterView.as_view(), name="account_signup"),
+    url(r'^accounts/signup/$', RegisterView.as_view(), name='account_signup'),
+    url(r'^accounts/login/$', views.LoginView.as_view(), name='account_login'),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^(?P<user_name>\w+)/', include('useraccounts.urls')),
 ]
