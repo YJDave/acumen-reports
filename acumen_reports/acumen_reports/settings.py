3242fc6917ab36ff57b698b02e02cb18acea6172
Parthvi Vala <parthvi.vala@gmail.com>
2018-05-03 15:19:21 +0530

    password toggle

--- acumen_reports/acumen_reports/settings.py
+++ acumen_reports/acumen_reports/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'django.contrib.sites',
     'multi_email_field',
     'useraccounts',
